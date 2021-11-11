95bed4e65485ec6cfec6e1b001e7759efad91509
Andrey Glushko <andrei19386@mail.ru>
2021-11-11 21:35:50 +0300

    The class Elevator is added to the project 04_Encapsulation

---
+++ 04_Encapsulation/01_Elevator/src/ru/skillbox/Elevator.java
@@ -0,0 +1,45 @@
+package ru.skillbox;
+
+public class Elevator {
+    private int currentFloor;
+    private int minFloor;
+    private int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor){ //Конструктор класса лифт
+        this.currentFloor = 1;
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        System.out.println("Вы находитесь на 1 этаже");
+    }
+    public int getCurrentFloor(){
+        return currentFloor;
+    }
+
+    public void moveDown(){
+        currentFloor = currentFloor > minFloor ? currentFloor - 1 : currentFloor;
+    }
+
+    public void moveUp(){
+        currentFloor = currentFloor < maxFloor ? currentFloor + 1 : currentFloor;
+    }
+
+    public void move(int floor){
+        if(floor > maxFloor){
+            System.out.println("Этаж задан некорректно! Вы не можете подняться выше " + maxFloor + " этажа");
+            return;
+        }
+        if(floor < minFloor){
+            System.out.println("Этаж задан некорректно! Вы не можете опуститься ниже " + minFloor + " этажа");
+            return;
+        }
+        while(currentFloor < floor){
+            moveUp();
+            System.out.println("Текущий этаж после перемещения - " + getCurrentFloor() );
+        }
+        while(currentFloor > floor){
+            moveDown();
+            System.out.println("Текущий этаж после перемещения - " + getCurrentFloor() );
+        }
+    }
+
+}
