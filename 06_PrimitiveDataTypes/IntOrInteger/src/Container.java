fdbbf8747b6f8b41541463b13d2a37512d5d9ed7
Andrey Glushko <andrei19386@mail.ru>
2021-11-18 19:43:16 +0300

    Integer is replaced by int. Russian letters are found

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,6 +1,7 @@
 public class Container {
-    private Integer count;
-
+    private int count;
+    //int - примитив, значение по умолчанию равно 0; Integer - класс-обертка, адрес по умолчанию равен NULL
+    //В данном случае, чтобы ошибка не возникала, нужно написать int, чтобы к значению 0 прибавить value
     public void addCount(int value) {
         count = count + value;
     }
