c2b8ed5251f2792486db26f4a6d55890e43776fc
Andrey Glushko <andrei19386@mail.ru>
2021-11-19 16:51:39 +0300

    Modified code for letters search

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -7,25 +7,45 @@ public class Main {
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
+        /*
+        Для поиска кодов русских букв воспользуемся тем, что все прописные буквы и все строчные буквы
+        в таблице кодов расположены в строгом соответствии с русским алфавитом (за исключением буквы ё).
+        Вначале найдем коды начальных и конечных букв алфавита а и я, а также прописных и строчных букв ё
+         */
+        int yoUppercaseCode = 0;
+        int yoLowercaseCode = 0;
+        int aUppercaseCode = 0;
+        int aLowercaseCode = 0;
+        int yaUppercaseCode = 0;
+        int yaLowercaseCode = 0;
+        int i = 0; //Переменная-параметр цикла
+        char c; // Вспомогательная переменная
 
-
-        //Буква Ё
-        int i = 1025;
-        char c = (char)i;
-        System.out.println(i + " - " + c);
-
-//Остальные заглавные и строчные буквы русского алфавита
-        //1040 - 1071 - прописные буквы
-        //1072 - 1103 - строчные буквы
-        for(i = 1040; i < 1104; i++){
+        for(i = 0; i < 65536; i++){ // Цикл по всей таблице символов
             c = (char)i;
-            System.out.println(i + " - " + c);
+            switch(c) {
+                case 'Ё' -> { yoUppercaseCode = i; }
+                case 'ё' -> { yoLowercaseCode = i; }
+                case 'А' -> { aUppercaseCode = i;  }
+                case 'а' -> { aLowercaseCode = i; }
+                case 'Я' -> { yaUppercaseCode = i;  }
+                case 'я' -> { yaLowercaseCode = i; }
+            }
         }
-
-        //Буква ё
-        i = 1105;
-        c = (char)i;
-        System.out.println(i + " - " + c);
-
+        //Выведем полученные результаты на экран и убеждаемся, что все символы алфавита найдены
+        System.out.println("Буква Ё - " + yoUppercaseCode);
+        System.out.println("Буква ё - " + yoLowercaseCode);
+        System.out.println("Буква А - " + aUppercaseCode);
+        for(i = aUppercaseCode + 1; i < yaUppercaseCode; i++){
+            c = (char)i;
+            System.out.println("Буква " + c + " - " + i);
+        }
+        System.out.println("Буква Я - " + yaUppercaseCode);
+        System.out.println("Буква а - " + aLowercaseCode);
+        for(i = aLowercaseCode + 1; i < yaLowercaseCode; i++){
+            c = (char)i;
+            System.out.println("Буква " + c + " - " + i);
+        }
+        System.out.println("Буква я - " + yaLowercaseCode);
     }
 }
