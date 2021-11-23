b668bbb2c63051f56a4ddb5da69ddf2a81b4b092
Andrey Glushko <andrei19386@mail.ru>
2021-11-23 19:55:24 +0300

    Homework 1 and 2 made

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -2,6 +2,52 @@ import java.util.Scanner;
 
 public class Main {
 
+  public static int spaceCount(String string) {
+    int count = 0;
+    for(int i = 0; i < string.length(); i++) {
+      if(string.charAt(i) == ' ') {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public static boolean isCyrillicText(String string) {
+    char symbol;
+    for(int i = 0; i < string.length(); i++) {
+      symbol = string.charAt(i);
+      if( (int)symbol >= 1040 && (int)symbol <= 1103 ) {
+        continue;
+      }
+      switch (symbol) {
+        case ' ' :  continue;
+        case '-' :  continue;
+        case 'ё' :  continue;
+        case 'Ё' :  continue;
+        default :  return false;
+      }
+    }
+    return true;
+  }
+
+  public static String getSurname(String string) {
+    int firstSpaceIndex = string.indexOf(' ', 0);
+    return string.substring(0,firstSpaceIndex);
+  }
+
+  public static String getName(String string) {
+    int firstSpaceIndex = string.indexOf(' ', 0);
+    int secondSpaceIndex = string.lastIndexOf(' ');
+    return string.substring(firstSpaceIndex + 1,secondSpaceIndex);
+  }
+
+
+  public static String gerPatronymic(String string) {
+    int secondSpaceIndex = string.lastIndexOf(' ');
+    return string.substring(secondSpaceIndex + 1, string.length());
+  }
+
+
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
@@ -10,8 +56,16 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      if( spaceCount(input.trim()) != 2 || !isCyrillicText(input.trim()) ) {
+        System.out.println("Введенная строка не является ФИО");
+        return;
+      }
+      String surname = getSurname(input.trim());
+      String name = getName(input.trim());
+      String patronymic = gerPatronymic(input.trim());
+
+      System.out.println("Фамилия: " + surname + "\nИмя: " + name + "\nОтчество: " + patronymic);
     }
   }
 
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,95 @@
 public class Main {
 
+  public static boolean isDigits(String substring){
+    for(int i = 0; i < substring.length(); i++){
+      if( !Character.isDigit(substring.charAt(i)) ) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean spaceExists(String string) {
+    char symbol;
+    for(int i = 0; i < string.trim().length(); i++){
+      symbol = string.trim().charAt(i);
+      if( symbol == ' ' ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int onlyOneWordAnalise(String string) {//Анализ единственного слова
+    String substring = string.trim().substring(0, string.trim().length());
+    if( isDigits(substring) ) {
+      return Integer.parseInt(substring);
+    }
+    return 0;
+  }
+
+  public static int firstWordAnalise(String string) {//Анализ первого слова на случай, если их несколько
+    int firstSpaceIndex = string.trim().indexOf(' ');
+    String substring = string.trim().substring(0, firstSpaceIndex);
+    if( isDigits(substring) ) {
+      return Integer.parseInt(substring);
+    }
+    return 0;
+  }
+
+
+  public static int lastWordAnalise(String string) {//Анализ последнего слова на случай, если их несколько
+    int lastSpaceIndex = string.trim().lastIndexOf(' ');
+    String substring = string.trim().substring(lastSpaceIndex + 1, string.trim().length());
+    if( isDigits(substring) ) {
+      return Integer.parseInt(substring);
+    }
+    return 0;
+  }
+
+  public static int middleWordsAnalise(String string) { //Анализ промежуточных слов на случай, если слов больше двух
+    int firstSpaceIndex = string.trim().indexOf(' ', 0);
+    int secondSpaceIndex;
+    int earning = 0;
+    int lastSpaceIndex = string.trim().lastIndexOf(' ');
+    while(firstSpaceIndex != lastSpaceIndex) {
+      secondSpaceIndex = string.trim().indexOf(' ', firstSpaceIndex + 1);
+      String substring = string.trim().substring(firstSpaceIndex + 1, secondSpaceIndex);
+      if( isDigits(substring) ) {
+        earning += Integer.parseInt(substring);
+      }
+      firstSpaceIndex = secondSpaceIndex;
+    }
+    return earning;
+  }
+
+
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+
+    int earning = 0;
+
+    if( text.trim().isEmpty() ) { //Если строка пустая, заработок равен 0
+      System.out.println("Суммарный заработок равен: " + earning);
+      return;
+    }
+
+    if(!spaceExists(text) ) { //Если нет пробелов, анализируем единственное слово
+      earning += onlyOneWordAnalise(text);
+      System.out.println("Суммарный заработок равен: " + earning);
+      return;
+    }
+
+    //Если есть хотя бы один пробел (хотя бы два слова, анализируем первое и последнее)
+    earning += firstWordAnalise(text);
+    earning += lastWordAnalise(text);
+    if(text.trim().indexOf(' ') != text.trim().lastIndexOf(' ')) {
+      //Кроме того, если больше одного пробела, анализируем слова в середине
+      earning += middleWordsAnalise(text);
+    }
+    System.out.println(earning);
+
   }
 
 }
