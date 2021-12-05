c5dac805e08acb80c848cd2f74b80cfea1e894a5
Andrey Glushko <andrei19386@mail.ru>
2021-12-05 16:36:35 +0300

    Homework 9.5 is done!

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,86 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
+    private static HashSet<String> uniqueNumbers = new HashSet<>();
+    private static TreeSet<String> sortedUniqueNumbers = new TreeSet<>();
+    public static final int TOTAL_NUMBERS = 10_000_000;
+
+    public static HashSet<String> getUniqueNumbers() {
+        return uniqueNumbers;
+    }
+
+    public static TreeSet<String> getSortedUniqueNumbers() {
+        return sortedUniqueNumbers;
+    }
+
+    private static char generateSymbol(){
+        int random = (int)( 12 * Math.random() );
+        switch (random) {
+            case 0: return 'А';
+            case 1: return 'В';
+            case 2: return 'Е';
+            case 3: return 'К';
+            case 4: return 'М';
+            case 5: return 'Н';
+            case 6: return 'О';
+            case 7: return 'Р';
+            case 8: return 'С';
+            case 9: return 'Т';
+            case 10: return 'У';
+            case 11: return 'Х';
+            default: return 'U';
+        }
+    }
+
+    private static String generateDigit(){
+        short random = (short) ( 10 * Math.random() );
+        return Short.toString(random);
+    }
+
+    private static String generateRegionNumber(){
+        short random = (short) ( 1000 * Math.random());
+        if(random < 10) {
+            return "00" + random;
+        } else if(random < 100) {
+            return "0" + random;
+        } else {
+            return Short.toString(random);
+        }
+    }
+
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        String coolNumber;
+        while(uniqueNumbers.size() < TOTAL_NUMBERS) {
+            coolNumber  = generateSymbol() + generateDigit() + generateDigit() + generateDigit() +
+                    generateSymbol() + generateSymbol() + generateRegionNumber();
+            uniqueNumbers.add(coolNumber);
+        }
+        List<String> resultList = new ArrayList<>();
+        for(String item : uniqueNumbers) {
+            resultList.add(item);
+            sortedUniqueNumbers.add(item);
+        }
+        return resultList;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        return list.contains(number);
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        int result = Collections.binarySearch(sortedList, number);
+        return  result >= 0;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class Main {
+    public static final String NUMBER = "Г057АЕ480"; // Константа для поиска заведомо отсутствующего номера
     /*
     TODO:
      - реализовать методы класса CoolNumbers
@@ -7,6 +10,47 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        List<String> coolNumbers = CoolNumbers.generateCoolNumbers();
+        String searchNumber = coolNumbers.get(9_000_000); // Поиск заведомо присутствующего номера
+        HashSet<String> uniqueNumbers = CoolNumbers.getUniqueNumbers();
+        TreeSet<String> sortedUniqueNumbers = CoolNumbers.getSortedUniqueNumbers();
 
+        long beforeSearch = System.currentTimeMillis();
+        boolean result = CoolNumbers.bruteForceSearchInList(coolNumbers, searchNumber);
+        //boolean result = CoolNumbers.bruteForceSearchInList(coolNumbers, NUMBER);
+        long afterSearch = System.currentTimeMillis();
+        long resultTime = afterSearch - beforeSearch;
+
+        Collections.sort(coolNumbers);
+
+        System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
+        System.out.println("Для поиска методом полного перебора время поиска составило " + resultTime + " мс");
+
+        beforeSearch = System.currentTimeMillis();
+        result = CoolNumbers.binarySearchInList(coolNumbers, searchNumber);
+        //result = CoolNumbers.binarySearchInList(coolNumbers, NUMBER);
+        afterSearch = System.currentTimeMillis();
+        resultTime = afterSearch - beforeSearch;
+        System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
+        System.out.println("Для бинарного поиска время поиска составило " + resultTime + " мс");
+
+        beforeSearch = System.currentTimeMillis();
+        result = CoolNumbers.searchInHashSet(uniqueNumbers, searchNumber);
+        //result = CoolNumbers.searchInHashSet(uniqueNumbers, NUMBER);
+
+        afterSearch = System.currentTimeMillis();
+        resultTime = afterSearch - beforeSearch;
+        System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
+        System.out.println("Для поиска в HashSet время поиска составило " + resultTime + " мс");
+
+
+        beforeSearch = System.currentTimeMillis();
+        result = CoolNumbers.searchInTreeSet(sortedUniqueNumbers, searchNumber);
+        //result = CoolNumbers.searchInTreeSet(sortedUniqueNumbers, NUMBER);
+
+        afterSearch = System.currentTimeMillis();
+        resultTime = afterSearch - beforeSearch;
+        System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
+        System.out.println("Для поиска в HashSet время поиска составило " + resultTime + " мс");
     }
 }
