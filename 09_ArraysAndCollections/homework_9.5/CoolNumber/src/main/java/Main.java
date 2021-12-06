fe7160ddfbd16f2ae2ef1b1a4b74e93e5e8d0b6d
Andrey Glushko <andrei19386@mail.ru>
2021-12-06 19:19:36 +0300

    Errors are fixed

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,13 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static final String NUMBER = "Г057АЕ480"; // Константа для поиска заведомо отсутствующего номера
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
+    public static final String NUMBER = "Г057АЕ480"; // Поиск заведомо отсутствующего номера
 
     public static void main(String[] args) {
         List<String> coolNumbers = CoolNumbers.generateCoolNumbers();
@@ -15,42 +9,42 @@ public class Main {
         HashSet<String> uniqueNumbers = CoolNumbers.getUniqueNumbers();
         TreeSet<String> sortedUniqueNumbers = CoolNumbers.getSortedUniqueNumbers();
 
-        long beforeSearch = System.currentTimeMillis();
+        long beforeSearch = System.nanoTime();
         boolean result = CoolNumbers.bruteForceSearchInList(coolNumbers, searchNumber);
         //boolean result = CoolNumbers.bruteForceSearchInList(coolNumbers, NUMBER);
-        long afterSearch = System.currentTimeMillis();
+        long afterSearch = System.nanoTime();
         long resultTime = afterSearch - beforeSearch;
 
         Collections.sort(coolNumbers);
 
         System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
-        System.out.println("Для поиска методом полного перебора время поиска составило " + resultTime + " мс");
+        System.out.println("Для поиска методом полного перебора время поиска составило " + resultTime + " нс");
 
-        beforeSearch = System.currentTimeMillis();
+        beforeSearch = System.nanoTime();
         result = CoolNumbers.binarySearchInList(coolNumbers, searchNumber);
         //result = CoolNumbers.binarySearchInList(coolNumbers, NUMBER);
-        afterSearch = System.currentTimeMillis();
+        afterSearch = System.nanoTime();
         resultTime = afterSearch - beforeSearch;
         System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
-        System.out.println("Для бинарного поиска время поиска составило " + resultTime + " мс");
+        System.out.println("Для бинарного поиска время поиска составило " + resultTime + " нс");
 
-        beforeSearch = System.currentTimeMillis();
+        beforeSearch = System.nanoTime();
         result = CoolNumbers.searchInHashSet(uniqueNumbers, searchNumber);
         //result = CoolNumbers.searchInHashSet(uniqueNumbers, NUMBER);
 
-        afterSearch = System.currentTimeMillis();
+        afterSearch = System.nanoTime();
         resultTime = afterSearch - beforeSearch;
         System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
-        System.out.println("Для поиска в HashSet время поиска составило " + resultTime + " мс");
+        System.out.println("Для поиска в HashSet время поиска составило " + resultTime + " нс");
 
 
-        beforeSearch = System.currentTimeMillis();
+        beforeSearch = System.nanoTime();
         result = CoolNumbers.searchInTreeSet(sortedUniqueNumbers, searchNumber);
         //result = CoolNumbers.searchInTreeSet(sortedUniqueNumbers, NUMBER);
 
-        afterSearch = System.currentTimeMillis();
+        afterSearch = System.nanoTime();
         resultTime = afterSearch - beforeSearch;
         System.out.println("Результат поиска - " + (result ? "Номер найден" : "Номер не найден") );
-        System.out.println("Для поиска в HashSet время поиска составило " + resultTime + " мс");
+        System.out.println("Для поиска в TreeSet время поиска составило " + resultTime + " нс");
     }
 }
