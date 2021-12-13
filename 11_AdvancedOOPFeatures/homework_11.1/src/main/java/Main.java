d59f87bbcc1ac0eb630f76c2fc1a111785d43a87
Andrey Glushko <andrei19386@mail.ru>
2021-12-13 19:24:58 +0300

    Homework 11.1 is done!

--- 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
+++ 11_AdvancedOOPFeatures/homework_11.1/src/main/java/Main.java
@@ -6,10 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        sortBySalaryAndAlphabet(staff);
         System.out.println(staff);
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        staff.sort((o1, o2) -> {
+            int resultCompareBySalary = o1.getSalary().compareTo(o2.getSalary());
+            if (resultCompareBySalary == 0) { //При равных зарплатах сортировка по имени
+                return o1.getName().compareTo(o2.getName());
+            } else {
+                return resultCompareBySalary;
+            }
+        });
     }
 }
