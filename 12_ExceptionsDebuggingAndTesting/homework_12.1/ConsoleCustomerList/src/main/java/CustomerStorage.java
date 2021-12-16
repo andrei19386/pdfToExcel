26db7b0dbe6cc7ee140235e673c80d0de5c2a9f2
Andrey Glushko <andrei19386@mail.ru>
2021-12-16 19:53:30 +0300

    Homework 13.1 is done!

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -15,8 +15,23 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        extractedExceptions(INDEX_EMAIL, INDEX_PHONE, components);
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+    }
+
+    private void extractedExceptions(int INDEX_EMAIL, int INDEX_PHONE, String[] components) {
+        if(components.length != 4) {
+            throw new IllegalArgumentException("Wrong format of input string! Please correct with format: " +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        if(!components[INDEX_EMAIL].matches("[A-z1-9]+@[A-z1-9]+.[A-z]{2,3}")){
+            throw new IllegalArgumentException("Wrong format of email! Please correct email");
+        }
+        if(!components[INDEX_PHONE].matches("[+][0-9]{11}")) {
+            throw new IllegalArgumentException("Wrong format of phone! Phone number must consist of " +
+                    "sign + and 11 digits! For example, +79261111111");
+        }
     }
 
     public void listCustomers() {
@@ -24,6 +39,9 @@ public class CustomerStorage {
     }
 
     public void removeCustomer(String name) {
+        if(getCustomer(name)==null) {
+            throw new IllegalArgumentException("Customer does not exist!");
+        }
         storage.remove(name);
     }
 
--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/Main.java
@@ -7,29 +7,47 @@ public class Main {
             "\tlist\n\tcount\n\tremove Василий Петров";
     private static final String COMMAND_ERROR = "Wrong command! Available command examples: \n" +
             COMMAND_EXAMPLES;
-    private static final String helpText = "Command examples:\n" + COMMAND_EXAMPLES;
+    private static final String HELP_TEXT = "Command examples:\n" + COMMAND_EXAMPLES;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
 
         while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+                if (tokens[0].equals("add")) {
+                    extractedExceptionAdd(tokens);
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    extractedExceptionRemove(tokens);
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(HELP_TEXT);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            } catch (IllegalArgumentException exception) {
+                exception.getMessage();
             }
+        }
+    }
+
+    private static void extractedExceptionRemove(String[] tokens) {
+        if (tokens.length == 1) {
+            throw new IllegalArgumentException("You should input the second argument to remove!");
+        }
+    }
+
+    private static void extractedExceptionAdd(String[] tokens) {
+        if (tokens.length == 1) {
+            throw new IllegalArgumentException("You should input the second argument to add!");
         }
     }
 }
