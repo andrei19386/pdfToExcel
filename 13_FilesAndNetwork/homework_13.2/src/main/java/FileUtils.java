1f80ca70ad7065e77eabff9f896a8c6d2de7dd7a
Andrey Glushko <andrei19386@mail.ru>
2021-12-24 19:48:48 +0300

    Homework 14.2 is done!

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,69 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try {
+            if(Path.of(destinationDirectory).toFile().exists()) {
+                if(!Path.of(destinationDirectory).toFile().canRead()) {
+                    throw new AccessDeniedException("The directory " + destinationDirectory + " cannot be read!");
+                }
+                if(!Path.of(destinationDirectory).toFile().canWrite()) {
+                    throw new AccessDeniedException("The directory " + destinationDirectory + " cannot be write!");
+                }
+                Stream<Path> filesDestination = Files.list(Path.of(destinationDirectory));
+                if (!filesDestination.collect(Collectors.toList()).isEmpty()) {
+                    throw new DirectoryNotEmptyException("The target directory is not empty!");
+                }
+                filesDestination.close();
+            }
+            if(!Path.of(sourceDirectory).toFile().exists()) {
+                throw new NoSuchFileException("The directory does not exists!");
+            }
+            if(!Path.of(sourceDirectory).toFile().canRead()) {
+                throw new AccessDeniedException("The directory " + sourceDirectory + " cannot be read!");
+            }
+            Files.copy(Path.of(sourceDirectory), Path.of(destinationDirectory), StandardCopyOption.REPLACE_EXISTING);
+            Stream<Path> files = Files.list(Path.of(sourceDirectory));
+            analyzerOfStream(destinationDirectory, files);
+            files.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void analyzerOfStream(String destinationDirectory, Stream<Path> files) {
+        files.forEach(file -> {
+            String newDestinationDirectory = destinationDirectory
+                    + "\\" + file.getFileName().toString();
+            if(!file.toFile().canRead()) {
+                try {
+                    throw new AccessDeniedException("The file or directory " +
+                            file.toAbsolutePath().toString() +
+                            " cannot be read!");
+                } catch (AccessDeniedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(file.toFile().isDirectory()){
+                copyFolder(file.toString(), newDestinationDirectory);
+            } else {
+                byte[] bytes = {};
+                try {
+                    bytes = Files.readAllBytes(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Files.write(Path.of(destinationDirectory + "\\" + file.getFileName().toString()),bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
+
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,17 @@
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите директорию, которую надо скопировать:");
+        String sourceDirectory = scanner.nextLine();
+        if(!Path.of(sourceDirectory).toFile().exists()) {
+            System.out.println("Директория не существует!");
+            return;
+        }
+        System.out.println("Введите директорию назначения:");
+        String destinationDirectory = scanner.nextLine();
+        FileUtils.copyFolder(sourceDirectory,destinationDirectory);
     }
 }
