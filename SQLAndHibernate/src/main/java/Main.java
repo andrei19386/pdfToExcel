6629a97fc1fba919ac989abe70f303f318fe939a
Andrey Glushko <andrei19386@mail.ru>
2022-01-17 19:30:45 +0300

    Homework 2 SQL is done!

--- SQLAndHibernate/src/main/java/Main.java
+++ SQLAndHibernate/src/main/java/Main.java
@@ -1,29 +1,35 @@
-import java.sql.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
-        String user = "root";
-        String pass = "123456789";
-        String query = "SELECT course_name, count(*)/8 AS Average_count_per_month FROM purchaselist \n" +
-                "WHERE year(subscription_date) = 2018 \n" +
-                "GROUP BY course_name";
-        try {
-            Connection connection = DriverManager.getConnection(url,user,pass);
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            System.out.println("course_name - Average_count_per_month");
-            while(resultSet.next()) {
-                String name  = resultSet.getString("course_name");
-                double averageCount = resultSet.getDouble("Average_count_per_month");
-                System.out.println(name + "  -  " + averageCount);
-            }
-            resultSet.close();
-            statement.close();
-            connection.close();
+        // Основное задание
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-        } catch (SQLException  e) {
-            e.printStackTrace();
+        Session session = sessionFactory.openSession();
+        Course course = session.get(Course.class,2);
+        System.out.println(course.getName() +  " - " + course.getStudentsCount());
+
+
+        //Дополнительное задание - необходимо раскомментировать цикл ниже, выводит записи 1-10 таблицы teachers
+        /*
+        for(int i = 1; i <= 10; i++) {
+            Teacher teacher = session.get(Teacher.class, i);
+            System.out.println("Id = " + teacher.getId() +
+                    ", name = " + teacher.getName() +
+                    ", salary = " + teacher.getSalary() +
+                    ", age = " + teacher.getAge());
         }
+        */
+
+        sessionFactory.close();
     }
 }
